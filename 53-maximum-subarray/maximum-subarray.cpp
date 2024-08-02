aefed8f60b7ddbb8f1a55e257f872a39b9df2a7f
Premchand Panku <pp21mmb0a51@student.nitw.ac.in>
2024-08-02 12:58:08 +0530

    Time: 83 ms (28.95%) | Memory: 70.6 MB (29.77%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-         int maxi = nums[0]; 
-        int sum = nums[0]; 
-        
-        for (int i = 1; i < nums.size(); i++) {
-            
-            sum = max(nums[i], sum + nums[i]);
-            
-            maxi = max(maxi, sum);
+       long long maxi = nums[0];  
+        long long sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
         }
+        return maxi;
         
-        return maxi; 
     }
 };
